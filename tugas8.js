d58e45aa7014abbd3dc03f0918442a6a06451053
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-14 08:33:36 +0800

    TUGAS 8 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function object() {
+  var artist = {
+    nama: 'Daniel Radcliffe',
+    umur: 31,
+    alamat: 'London',
+
+  }
+  console.log(artist)
+  artist.alamat = 'New York';
+
+  console.log(artist)
+}
+
+object()
